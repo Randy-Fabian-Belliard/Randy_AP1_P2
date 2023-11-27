e03784c133c5e72651100f9cbac00883387aee01
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2023-11-27 00:23:58 -0400

    Agregando notificaciones

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Parcial2_AP1_Randy.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
